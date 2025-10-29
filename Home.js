b09ab32742ce3d90038fe64aa52ee3abf00ce6a7
Tres238 <veratreasure238@gmail.com>
2025-10-29 08:43:18 +0100

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home() {
+  return (
+    <main>
+      <h2>Welcome to TicketEase 🎫</h2>
+      <p>
+        TicketEase helps you manage and track support tickets efficiently.
+        Create, monitor, and close tickets easily with a simple dashboard.
+      </p>
+      <button className="btn-primary">Get Started</button>
+    </main>
+  );
+}
+
+export default Home;
